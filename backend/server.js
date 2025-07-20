8bf3171b6a836807bf6d2b7b1dd22f05b24abbed
Ajaysriram13 <ajaysriram16@gmail.com>
2025-07-20 16:46:20 +0530

    Fixing the cors error

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,11 @@ connectDB();
 startCronJobs();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://remotemessagesenderfrontend.onrender.com",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 app.use(express.json());
 
 const connectedUsers = {}; // userId -> socketId
